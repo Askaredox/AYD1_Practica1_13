556823b263de9b3cd957b31be3d25a9ad715d60a
Lester Mazariegos <lestermaz.96@gmail.com>
2021-06-07 17:16:32 -0600

    Se agregaron los datos del estudiante 3

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -35,7 +35,8 @@ class Home extends React.Component{
             ],
             filas:[
                 {id:'201612272', name:'Andrés Esteban', lastname:'Carvajal Morales', mail:'andyecarvajal@gmail.com', phone:'5241-4066'},
-                {id:'201325533', name:'Erick Fernando', lastname:'Elias Diaz', mail:'elias25533@gmail.com', phone:'4574-7474'}
+                {id:'201325533', name:'Erick Fernando', lastname:'Elias Diaz', mail:'elias25533@gmail.com', phone:'4574-7474'},
+                {id:'201403610', name:'Lester Fernando', lastname:'Mazariegos Navarro', mail:'lestermaz.96@gmail.com', phone:'3405-0585'}
             ]
         }
     }
